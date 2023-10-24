ba9085453f226076aebb1cbb4af506a37a422a58
escanor24k <k.buebel@hotmail.com>
2023-10-24 18:18:54 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,10 @@
+function navbar(){
+    const selectButton = document.getElementById('menuBar');
+    if(selectButton.classList.contains('hidden'))
+    {
+        selectButton.classList.replace('hidden', 'popNav')
+    }
+    else{
+        selectButton.classList.replace('popNav', 'hidden')
+    }
+}
